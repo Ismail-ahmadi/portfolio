684833197007263c43558eedbfa7d9397632cc48
Ismail-ahmadi <ahmadi.ismail92@gmail.com>
2023-01-02 18:47:18 -0500

    fixed the nav names

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -36,9 +36,9 @@ function Nav() {
           <li><a href="#">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#experience">Experience</a></li>
-          <li><a href="#services">Services</a></li>
+          <li><a href="#services">Qualification</a></li>
           <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#testimonials">Testimonials</a></li>
+          <li><a href="#testimonials">Recommendation</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </div>
